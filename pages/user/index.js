ac9ed19418f0f21a5c875732604e08e73e384096
Harsha Vardhan <hv2003789@gmail.com>
2023-03-17 15:01:16 +0530

    Typo in calling UserNavbar component

--- pages/user/index.js
+++ pages/user/index.js
@@ -1,7 +1,7 @@
 import style from "../../styles/User.module.css";
 import Image from "next/image";
 import Drone from "../../assets/DroneDelivery-pana.svg";
-import NavBar from "../../components/NavBar/UserNavbar";
+import NavBar from "../../components/Navbar/UserNavbar";
 
 const UserHome = () => {
   return (
